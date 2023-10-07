6f460c92f438be2f936a332a78ab30e71c7de605
Tanner Hart <tannerhart24@yahoo.com>
2023-10-06 17:18:43 -0700

    Refactored to allow for global access to the parsed data.

--- maps.js
+++ maps.js
@@ -9,6 +9,7 @@ let map;
 // let rows; // Holds a list of each row in the csv
 let coordinates = []; // Holds a list of all coordinate pairs for each track
 let locations = [];
+let parsedData = [];
 
 async function initMap() {
     // const pos = { lat:  39.776326, lng: -121.8676 }; // Set coordinates for the pin, Method Marketing.
@@ -37,24 +38,23 @@ async function initMap() {
         .then(res => res.text())
         .then(data => {
             // Lists that are correct
-            let rows = data.split('\n').map(row => row.trim()).filter(row => row.length); // Splits each row into its own element, trims whitespace, and removes any empty rows
-            let parsedData = rows.slice(1).map(row => {
+            let rows = data.split('\n').map(row => row.trim()).filter(row => row.length); // Splits each row into its own element, trims whitespace, and filters any empty rows
+            parsedData = rows.slice(1).map(row => {
                 const columns = row.split(',');
                 return columns.map(column => 
-                    column.replace(/^"|"$/g, '').replace(/&/g, ' ').replace(/%/g, ',')
+                    column.replace(/^"|"$/g, '').replace(/%/g, ',')
                 );
             });
             
             // Parsing the formatted data for the coordinates and 
             // coordinates = parsedData.map(item => [+item[5], +item[6]]); // Includes tracks that have 'null' for the coordinates and stores the values as floats
-            coordinates = parsedData.filter(item => item[5] !== 'null' && item[6] !== 'null').map(item => [+item[5], +item[6]]); // Omits tracks that do not have coordinates and stores the values as a float
+            coordinates = parsedData.filter(item => item[5] !== 'null' && item[6] !== 'null').map(item => [+item[5], +item[6]]); // Omits tracks that do not have coordinates and converts values to a float
             
-            
-            console.log("\Parsed Data\n", parsedData);
-            console.log(coordinates)
+            locations = parsedData.map(item => item[0]);
         })
         .catch(err => console.error(err)
     );
+    // console.log("\Parsed Data\n", parsedData);
     
 
 
@@ -62,15 +62,22 @@ async function initMap() {
     let marker;
     for (let i = 0; i < coordinates.length; i++) {
         marker = new google.maps.Marker({
-            // position: new google.maps.LatLng(coordinates[i][1], coordinates[i][2]),
             position: new google.maps.LatLng(coordinates[i][0], coordinates[i][1]),
             map: map,
-            // title: locations[i][0]
+            // icon: {
+            //     path: 'M -2,0 0,-2 2,0 0,2 z',
+            //     fillColor: '#FF0000',
+            //     fillOpacity: 1,
+            //     scale: 1
+            // }
+            title: locations[i],
+            // opacity: .5
         });
         google.maps.event.addListener(marker, 'click', (function (marker, i) {
             return function () {
-                infowindow.setContent(locations[i][0]);
+                infowindow.setContent(locations[i]);
                 infowindow.open(map, marker);
+                // marker.setOpacity(1)
             }
         })(marker, i));
   }
