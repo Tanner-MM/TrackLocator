a4d675aa7a01f43496625bfe28af93ce8bd489cc
Tanner Hart <tannerhart24@yahoo.com>
2023-10-25 16:38:29 -0700

    Removed radius filter feature and implemented search bar using google places api. Created dynamic zoom depending on submission.

--- functions.js
+++ functions.js
@@ -68,14 +68,11 @@ function generateCardsAndPlaceMarkers() {
         map: map,
         title: locations[index]
       });
-    //   marker.trackCard = trackEl;
+    //   marker.trackCard = trackEl; // Creates a custom property on the marker to link it its corresponding track card
       markers.push(marker);
-  
 
       createInfoWindow(marker, index); // Create the infowindow for each marker.
-  
       trackContainer.appendChild(trackEl);
-
       trackEl.marker = marker; // Binds the marker to the track card
     });
   }
@@ -112,69 +109,10 @@ function generateCardInfoAndClickListeners() {
       }
   }));
 
-  //generateCards(); // Generates the info card elements
   generateCardsAndPlaceMarkers();
   createClickListeners();
 }
 
-// async function focusOnMarker(locationId) {
-//   for (let marker of markers) {
-//       if (marker.get('title') === locationId) {
-//           map.setCenter(marker.getPosition(), 1);
-//           marker.setAnimation(google.maps.Animation.BOUNCE);
-//           setTimeout(() => marker.setAnimation(null), 1000); // Bounce animation for 1s to indicate which is selected
-
-//           // Open the corresponding infoWindow
-//           if (markerInfoWindows[locationId]) {
-//               if (lastOpenedInfoWindow) {
-//                   lastOpenedInfoWindow.close();
-//               }
-//               markerInfoWindows[locationId].setContent(locations[markers.indexOf(marker)]);
-//               markerInfoWindows[locationId].open(map, marker);
-//               lastOpenedInfoWindow = markerInfoWindows[locationId];
-//           }
-
-//           let trackCard = document.querySelector(`track-card[data-name="${locationId}"]`);
-//           if (trackCard) {
-//               trackCard.scrollIntoView({ behavior: 'smooth', block: 'center' });
-//               if (currentHighlightedCard)
-//                   currentHighlightedCard.card.classList.remove('highlighted');
-//               trackCard.card.classList.add('highlighted');
-//               currentHighlightedCard = trackCard;
-//           }
-//       }
-//   }
-// }
-
-// async function focusOnMarker(locationId) {
-//     let trackCard = document.querySelector(`track-card[data-name="${locationId}"]`);
-    
-//     if (trackCard) {
-//       let marker = trackCard.marker;
-      
-//       if (marker) {
-//         map.setCenter(marker.getPosition(), 1);
-//         marker.setAnimation(google.maps.Animation.BOUNCE);
-//         setTimeout(() => marker.setAnimation(null), 1000); // Bounce animation for 1s to indicate which is selected
-  
-//         // Open the corresponding infoWindow
-//         if (markerInfoWindows[locationId]) {
-//           if (lastOpenedInfoWindow) {
-//             lastOpenedInfoWindow.close();
-//           }
-//           markerInfoWindows[locationId].setContent(locations[markers.indexOf(marker)]);
-//           markerInfoWindows[locationId].open(map, marker);
-//           lastOpenedInfoWindow = markerInfoWindows[locationId];
-//         }
-      
-//         trackCard.scrollIntoView({ behavior: 'smooth', block: 'center' });
-//         if (currentHighlightedCard)
-//           currentHighlightedCard.card.classList.remove('highlighted');
-//         trackCard.card.classList.add('highlighted');
-//         currentHighlightedCard = trackCard;
-//       }
-//     }
-//   }
 
 async function focusOnMarker(locationId) {
     let trackCard = document.querySelector(`track-card[data-name="${locationId}"]`);
@@ -226,56 +164,55 @@ function createClickListeners() {
   }
 }
 
-function haversineDistance(lat1, lon1, lat2, lon2) {
-  var R = 6371; // Radius of the Earth in kilometers
-  var dLat = (lat2 - lat1) * Math.PI / 180;
-  var dLon = (lon2 - lon1) * Math.PI / 180;
-  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-      Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return R * c; // Distance in kilometers
-}
+// function haversineDistance(lat1, lon1, lat2, lon2) {
+//   var R = 6371; // Radius of the Earth in kilometers
+//   var dLat = (lat2 - lat1) * Math.PI / 180;
+//   var dLon = (lon2 - lon1) * Math.PI / 180;
+//   var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+//       Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+//       Math.sin(dLon / 2) * Math.sin(dLon / 2);
+//   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//   return R * c; // Distance in kilometers
+// }
 
 
-function applyRadiusFilter() {
-  const center = map.getCenter();
-  const starterLocation = { lat: center.lat(), lng: center.lng() };
+// function applyRadiusFilter() {
+//   const center = map.getCenter();
+//   const starterLocation = { lat: center.lat(), lng: center.lng() };
   
-  const visibleTracks = tracks.filter(track => {
-      const distance = haversineDistance(
-          starterLocation.lat, starterLocation.lng,
-          track.coordinates.lat, track.coordinates.lng
-      );
-      return radiusDropdown.value == "all" || distance <= parseFloat(radiusDropdown.value);
-  });
-  // Hide or show markers based on the filtered tracks
-  for (let marker of markers) {
-      if (visibleTracks.some(track => track.trackName === marker.get('title'))) {
-          marker.setMap(map);
-      } else {
-          marker.setMap(null);
-      }
-  }
+//   const visibleTracks = tracks.filter(track => {
+//       const distance = haversineDistance(
+//           starterLocation.lat, starterLocation.lng,
+//           track.coordinates.lat, track.coordinates.lng
+//       );
+//       return radiusDropdown.value == "all" || distance <= parseFloat(radiusDropdown.value);
+//   });
+//   // Hide or show markers based on the filtered tracks
+//   for (let marker of markers) {
+//       if (visibleTracks.some(track => track.trackName === marker.get('title'))) {
+//           marker.setMap(map);
+//       } else {
+//           marker.setMap(null);
+//       }
+//   }
 
-  // Hide or show track cards based on the filtered tracks
-  let cards = document.querySelectorAll('track-card');
-  for (let card of cards) {
-      if (visibleTracks.some(track => track.trackName === card.getAttribute('data-name'))) {
-          card.style.display = 'block';
-      } else {
-          card.style.display = 'none';
-      }
-  }
-  noResultsMessage.style.display = visibleTracks.length == 0 ? 'block' : 'none';
-  
-}
+//   // Hide or show track cards based on the filtered tracks
+//   let cards = document.querySelectorAll('track-card');
+//   for (let card of cards) {
+//       if (visibleTracks.some(track => track.trackName === card.getAttribute('data-name'))) {
+//           card.style.display = 'block';
+//       } else {
+//           card.style.display = 'none';
+//       }
+//   }
+//   noResultsMessage.style.display = visibleTracks.length == 0 ? 'block' : 'none';
+// }
 
 
-function removeFilters() {
-  radiusDropdown.value = "all";
-  applyRadiusFilter();
-}
+// function removeFilters() {
+//   radiusDropdown.value = "all";
+//   applyRadiusFilter();
+// }
 
 function getUserLocation() {
   return new Promise((resolve, reject) => {
@@ -294,54 +231,85 @@ function getUserLocation() {
   });
 }
 
+function createDocumentListeners() {
+    document.getElementById('location-search').addEventListener('input', function () {
+        if (this.value) {
+            document.getElementById('resetButton').style.display = 'block';
+        } else {
+            document.getElementById('resetButton').style.display = 'none';
+        }
+    });
 
-// function placeMarkers() {
-//   for(let i = 0; i < coordinates.length; i++){
-//     let marker = new google.maps.Marker({
-//           position: new google.maps.LatLng(coordinates[i][0], coordinates[i][1]),
-//           map: map,
-//           title: locations[i]
-//       });
-//       markers.push(marker);
-//       createInfoWindow(marker, i);
-//   }
-// }
-// function generateCards() {
-//     const trackContainer = document.getElementById("track-container");
-  
-//     while (trackContainer.firstChild) {
-//         trackContainer.removeChild(trackContainer.firstChild);
-//     }
-  
-//     tracks.forEach(track => {
-//         const trackEl = document.createElement("track-card");
-//         trackEl.setAttribute('data-name', track.trackName);
-//         for (let [key, value] of Object.entries(track)) {
-//             let propEl = document.createElement("span");
-//             if (!!value) { // Check for missing value. If falsy, skip it.
-//                 propEl.setAttribute("slot", key);
-//                 propEl.textContent = value;
-//                 trackEl.appendChild(propEl);
-//             }
-//         }
-//         trackEl.marker = createMarker()
-  
-//         trackContainer.appendChild(trackEl);
-//     });
-//   }
-// function createMarker({ lat, lng }) {
-//     if (!lat || !lng) {
-//         throw new Error("Latitude and Longitude are required to create a marker.");
-//     }
+    document.getElementById('resetButton').addEventListener('click', function () {
+        const autocompleteInput = document.getElementById('location-search');
+        autocompleteInput.value = '';
+        this.style.display = 'none';
+        document.getElementById('no-results-message').style.display = 'none'; // Hide the no locations message if it's shown
+    });
 
-//     if (!map || !(map instanceof google.maps.Map)) {
-//         throw new Error("A valid Google Maps instance is required to create a marker.");
-//     }
 
-//     const marker = new google.maps.Marker({
-//         position: { lat, lng },
-//         map: map
-//     });
+    document.addEventListener('DOMContentLoaded', function () {
 
-//     return marker;
-// }
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 39.84181336054336, lng: -99.90822182318774 }, // Default coordinates
+            zoom: 8
+        });
+
+        // Initialize the Autocomplete functionality
+        const autocomplete = new google.maps.places.Autocomplete(
+            (document.getElementById('location-search')),
+            { types: ['geocode'] }
+        );
+
+        // Bind the map's bounds (viewport) property to the autocomplete object,
+        // so the autocomplete's boundary is automatically adjusted based on the map's bounds.
+        autocomplete.bindTo('bounds', map);
+
+        // Set up the listener for the autocomplete widget
+        autocomplete.addListener('place_changed', function () {
+            const place = autocomplete.getPlace();
+
+            if (place.geometry) {
+                // map.panTo(place.geometry.location);
+                map.setCenter(place.geometry.location);
+
+                map.setZoom(_getZoomLevel(place.types));
+            } else {
+                document.getElementById('location-search').placeholder = 'Enter a location';
+
+            }
+        });
+    });
+}
+
+
+function _getZoomLevel(placeTypes) {
+    const zoomLevels = {
+        'country': 5,
+        'administrative_area_level_1': 6,  // State/Province level
+        'administrative_area_level_2': 8,  // County level
+        'administrative_area_level_3': 10,
+        'locality': 12,                    // City level
+        'sublocality': 14,
+        'neighborhood': 15,
+        'route': 16,                       // Streets in a city
+        'street_address': 17,              // Specific address
+        'premise': 18,                     // Specific building
+        'natural_feature': 10,
+        'airport': 13,
+        'park': 15,
+        'point_of_interest': 16
+    };
+    // Default to a mid-range zoom level
+    let zoom = 12;
+
+    for (let type of placeTypes) {
+        if (zoomLevels[type]) {
+            zoom = zoomLevels[type];
+            break;  // Use the first matched type to determine zoom
+        }
+    }
+
+    return zoom;
+}
